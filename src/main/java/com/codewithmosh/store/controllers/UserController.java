2b8cbaab59d2676c23ed3a65523d02fcd690132d
Satyam0004 <satyamkmr04@gmail.com>
2025-10-12 20:03:52 +0530

    validating buisness rules

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -55,9 +55,15 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
+    public ResponseEntity<?> createUser(
             @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already registered")
+            );
+        }
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
