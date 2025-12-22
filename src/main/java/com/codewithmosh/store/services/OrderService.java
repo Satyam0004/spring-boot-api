49063cd6bee030bad41dc82dc49ec4f4758918e6
Satyam0004 <satyamkmr04@gmail.com>
2025-12-22 13:05:49 +0530

    getting single orders

--- src/main/java/com/codewithmosh/store/services/OrderService.java
+++ src/main/java/com/codewithmosh/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.codewithmosh.store.services;
 
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.mappers.OrderMapper;
 import com.codewithmosh.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +17,27 @@ public class OrderService {
     private final OrderMapper orderMapper;
     private final AuthService authService;
 
+    private static RuntimeException get() {
+        throw new OrderNotFoundException();
+    }
+
 
     public List<OrderDto> getAllOrders() {
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrderByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository
+                .getOrderWithItems(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+
+        var user = authService.getCurrentUser();
+
+        if(!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You are not allowed to access this order");
+        }
+        return orderMapper.toDto(order);
     }
 }
