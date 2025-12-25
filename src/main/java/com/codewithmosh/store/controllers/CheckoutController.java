ed5385d8ab1603532c5cbf7702df0178328828c8
Satyam0004 <satyamkmr04@gmail.com>
2025-12-25 18:13:59 +0530

    some updates(minor)

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -3,30 +3,21 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
-import com.codewithmosh.store.entities.Order;
-import com.codewithmosh.store.entities.OrderItem;
 import com.codewithmosh.store.entities.OrderStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
-import com.codewithmosh.store.repositories.CartRepository;
 import com.codewithmosh.store.repositories.OrderRepository;
-import com.codewithmosh.store.services.AuthService;
-import com.codewithmosh.store.services.CartService;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.SignatureVerificationException;
-import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -57,7 +48,9 @@ public class CheckoutController {
             switch (event.getType()) {
                 case "payment_intent.succeeded" -> {
                     // Update order status (PAID) ->
-                    // stripe trigger payment_intent.succeeded --add "payment_intent:metadata[order_id]=19"
+                    // stripe login
+                    // stripe listen --forward-to http://localhost:8080/checkout/webhook(for stripe server) stripe CLI
+                    // stripe trigger payment_intent.succeeded --add "payment_intent:metadata[order_id]=?"
                     // trigger this in cmd
                     var paymentIntent = (PaymentIntent) stripeObject;
                     if(paymentIntent != null) {
